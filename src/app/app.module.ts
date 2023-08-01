61fec5c778a3c20abc4baec65f30370e12c8b9b9
DKATZZZ <92221627+DKATZZZ@users.noreply.github.com>
2023-08-01 16:48:24 +0530

    commit all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { CounterButtonsComponent } from './components/counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './components/counter-output/counter-output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterButtonsComponent,
+    CounterOutputComponent
   ],
   imports: [
     BrowserModule
