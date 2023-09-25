8359531323138582f766d61b7fe03b097edf927a
DKATZZZ <92221627+DKATZZZ@users.noreply.github.com>
2023-09-25 21:53:32 +0530

    commit protect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 
@@ -18,6 +19,7 @@ const routes:Routes = [
     path:'posts',
       loadChildren: () =>
          import('./posts/post.module').then((m)=>m.PostModule),
+         canActivate:[AuthGuard],
   },
   {
     path: 'auth',
